26eeb67dcdaa4dceb3ef26130abbaf282e537f50
Jacob Sullenszino <JacobSz@mail.com>
2023-11-03 16:58:28 -0700

    init!!!

--- app.py
+++ app.py
@@ -1,112 +1,115 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import sqlite3
+import json
 
 def create_connection():
-  connection = sqlite3.connect("books.db")
-  return connection
+    connection = sqlite3.connect("bmn.db")
+    return connection
 
 def create_table():
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("""
- CREATE TABLE IF NOT EXISTS books (
- id INTEGER PRIMARY KEY AUTOINCREMENT,
- title TEXT NOT NULL,
- author TEXT NOT NULL
- )
- """)
- connection.commit()
- connection.close()
-
-create_table() # Call this function to create the table
-
-class BookCreate(BaseModel):
-  title: str
-  author: str
-
-class Book(BookCreate):
-  id: int
-
-def get_all_books():
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books")
-    books = cursor.fetchall()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS bmns (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        bmn_data TEXT NOT NULL
+    )
+    """)
+    connection.commit()
     connection.close()
-    return books
 
-def create_book(book: BookCreate):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("INSERT INTO books (title, author) VALUES (?, ?)", (book.title, book.author))
- connection.commit()
- connection.close()
- book_id = cursor.lastrowid
+create_table()  # Call this function to create the table
 
- connection.close()
- return book_id
+class BMNCreate(BaseModel):
+    title: str
+    lengthy_description: str
+    image_1: str
+    image_2: str
+    date_watched: str
 
-def get_book_by_id(book_id: int):
+class BMN(BMNCreate):
+    id: int
+
+def get_all_bmns():
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books WHERE id = ?", (book_id,))
-    book = cursor.fetchone()
+    cursor.execute("SELECT id, bmn_data FROM bmns")
+    bmns = cursor.fetchall()
     connection.close()
-    if book:
-        return book
+
+    bmns_list = []
+    for bmn in bmns:
+        bmn_dict = {"id": bmn[0], **json.loads(bmn[1])}
+        bmns_list.append(bmn_dict)
+
+    return bmns_list
+
+def create_bmn(bmn: BMNCreate):
+    connection = create_connection()
+    cursor = connection.cursor()
+    bmn_data = json.dumps({"title": bmn.title, "lengthy_description": bmn.lengthy_description, "image_1": bmn.image_1, "image_2": bmn.image_2, "date_watched": bmn.date_watched, })
+    cursor.execute("INSERT INTO bmns (bmn_data) VALUES (?)", (bmn_data,))
+    connection.commit()
+    bmn_id = cursor.lastrowid
+    connection.close()
+    return bmn_id
+
+def get_bmn_by_id(bmn_id: int):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT bmn_data FROM bmns WHERE id = ?", (bmn_id,))
+    bmn = cursor.fetchone()
+    connection.close()
+    if bmn:
+        return json.loads(bmn[0])
     else:
-        raise HTTPException(status_code=404, detail="Book not found")
+        raise HTTPException(status_code=404, detail="BMN not found")
 
-# Function to update a book by its ID
-def update_book(book_id: int, book: BookCreate):
+def update_bmn(bmn_id: int, bmn: BMNCreate):
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("UPDATE books SET title = ?, author = ? WHERE id = ?", (book.title, book.author, book_id))
+    bmn_data = json.dumps({"title": bmn.title, "lengthy_description": bmn.lengthy_description, "image_1": bmn.image_1, "image_2": bmn.image_2, "date_watched": bmn.date_watched, })
+    cursor.execute("UPDATE bmns SET bmn_data = ? WHERE id = ?", (bmn_data, bmn_id))
     connection.commit()
     connection.close()
 
-# Function to delete a book by its ID
-def delete_book(book_id: int):
+def delete_bmn(bmn_id: int):
     connection = create_connection()
     cursor = connection.cursor()
-    cursor.execute("DELETE FROM books WHERE id = ?", (book_id,))
+    cursor.execute("DELETE FROM bmns WHERE id = ?", (bmn_id,))
     connection.commit()
     connection.close()
-
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return {"message": "Welcome to the CRUD API"}
+    return {"message": "Welcome to the CRUD API"}
 
-@app.get("/books/")
-def get_all_books_endpoint():
-    all_books = get_all_books()
-    return {"books": all_books}
+@app.get("/bmns/")
+def get_all_bmns_endpoint():
+    all_bmns = get_all_bmns()
+    return {"bmns": all_bmns}
 
-@app.post("/books/")
-def create_book_endpoint(book: BookCreate):
- book_id = create_book(book)
- return {"id": book_id, **book.model_dump()}
+@app.post("/bmns/")
+def create_bmn_endpoint(bmn: BMNCreate):
+    bmn_id = create_bmn(bmn)
+    return {"id": bmn_id, "title": bmn.title, "lengthy_description": bmn.lengthy_description, "image_1": bmn.image_1, "image_2": bmn.image_2, "date_watched": bmn.date_watched, }
 
-# Endpoint to get a single book by its ID
-@app.get("/books/{book_id}")
-def get_book(book_id: int):
-    book = get_book_by_id(book_id)
-    return {"book": book}
+@app.get("/bmns/{bmn_id}")
+def get_bmn(bmn_id: int):
+    bmn = get_bmn_by_id(bmn_id)
+    return {"bmn": bmn}
 
-# Endpoint to update a book by its ID
-@app.put("/books/{book_id}")
-def update_book_endpoint(book_id: int, book: BookCreate):
-    get_book_by_id(book_id)  # Check if the book exists, raises 404 if not found
-    update_book(book_id, book)
-    return {"book": book, "id":book_id}
+@app.put("/bmns/{bmn_id}")
+def update_bmn_endpoint(bmn_id: int, bmn: BMNCreate):
+    get_bmn_by_id(bmn_id)
+    update_bmn(bmn_id, bmn)
+    return {"message": "BMN updated successfully", "id": bmn_id, "content": bmn}
 
-# Endpoint to delete a book by its ID
-@app.delete("/books/{book_id}")
-def delete_book_endpoint(book_id: int):
-    get_book_by_id(book_id)  # Check if the book exists, raises 404 if not found
-    delete_book(book_id)
-    return {"message": "Book deleted successfully"}
+@app.delete("/bmns/{bmn_id}")
+def delete_bmn_endpoint(bmn_id: int):
+    get_bmn_by_id(bmn_id)
+    delete_bmn(bmn_id)
+    return {"message": "BMN deleted successfully"}
